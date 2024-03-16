e0b2ded08cb0a35cf8af69bd5e2c9d240a512cee
JUNGmoon123 <jungpro567@gmail.com>
2024-03-16 18:21:40 +0900

    과제2

---
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -0,0 +1,53 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.vo.Article;
+
+@Mapper
+public interface ArticleRepository {
+
+	@Insert("""
+			INSERT INTO
+			article SET
+			regDate = NOW(),
+			updateDate = NOW(),
+			memberId = #{memberId},
+			title = #{title}, `body` = #{body}
+			""")
+	public void writeArticle(int memberId, String title, String body);
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
+
+	@Select("""
+			SELECT *
+			FROM article
+			WHERE id = #{id}
+			""")
+	public Article getArticle(int id);
+
+	@Select("""
+			SELECT A.*, M.nickname AS extra__writer
+			FROM article AS A
+			INNER JOIN `member` AS M
+			ON A.memberId = M.id
+			WHERE A.id = #{id}
+				""")
+	public Article getForPrintArticle(int id);
+
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(int id);
+
+//	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
+	public void modifyArticle(int id, String title, String body);
+
+//	@Select("SELECT * FROM article ORDER BY id DESC")
+	public List<Article> getArticles();
+
+}
